ae856ae04b10add06671e70b74032f89d8e56a1c
Kanika Chouhan <kanikachouhan@Kanikas-MacBook-Pro.local>
2023-06-02 19:28:23 +0530

    predicting with graph

--- data.py
+++ data.py
@@ -1,11 +1,16 @@
 # import the libraries 
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
+import time
 import numpy as np
 from sklearn.model_selection import train_test_split
 # from sklearn import cross_validation
 from sklearn import preprocessing, svm #scaling data, feature to be between -1 and +1. 
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style 
+
+style.use('ggplot')
 
 # look for the dataset and find the ticker
 #df = quandl.get('BATS/EDGA_TGH_PB')
@@ -49,16 +54,19 @@ df['label']= df[forecast_col].shift(-forecast_out) #shifting the columns negativ
 #the label column for each row would be  adjusted close price 10 days into the future 
 
 
-df.dropna(inplace=True)
+
 
 #features 
 X = np.array(df.drop(['label'], axis=1))
 #labels 
-y= np.array(df['label'])
-
-print(len(X), len(y))
+# y= np.array(df['label'])
 
 X= preprocessing.scale(X)
+X = X[:-forecast_out] #to the point of negative forecastout 
+X_lately = X[-forecast_out:]
+df.dropna(inplace=True)
+
+y= np.array(df['label'])
 y= np.array(df['label'])
 
 
@@ -71,6 +79,30 @@ clf= LinearRegression(n_jobs=10) #to change algo, just switch linear regresion t
 clf.fit(X_train, y_train) #fit= train 
 accuracy = clf.score(X_test, y_test) #score = test
 
-print(accuracy)
+# print(accuracy)
 # accuarcy in linear regression is the squared error 
+
+forecast_set = clf.predict(X_lately) #single value or an array of value to predict 
+print(forecast_set, accuracy, forecast_out)
+
+df['Date'] = pd.to_datetime(df.index) 
+df['Forecast'] = np.nan
+
+last_date = df['Date'].iloc[-1]
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Close'].plot()
+
+df ['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
 
